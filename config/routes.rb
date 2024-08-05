d3ecfd345a7961f83445b12bcfcafd26119dfae4
shar0ff <sharovivan17@gmail.com>
2024-08-05 17:50:11 +0200

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root "static_pages#home"
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
