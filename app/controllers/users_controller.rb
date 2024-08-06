f737ea121c23d2177dc3b0af74396d8fdd28c5fb
shar0ff <sharovivan17@gmail.com>
2024-08-06 22:58:40 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
     else
       render 'new'
     end
