c8af29ffc2f83eb6cd634951239744c48a557d95
shar0ff <sharovivan17@gmail.com>
2024-08-07 22:09:31 +0200

    Finish log out

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   get 'login'  => 'sessions#new'
   post 'login'  => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  get 'logout' => 'sessions#destroy'
 
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
