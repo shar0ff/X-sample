cfe7e6236a2fd8fda0a203c305887de54666a5dc
shar0ff <sharovivan17@gmail.com>
2024-08-12 19:13:27 +0200

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
